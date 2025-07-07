2a0f439f0ec96ca556feb45e56b0fabd8faba882
Shmahailo <yulifeya@ukr.netgit cleargit config --global user.email yulifeya@ukr.net>
2025-07-07 13:17:19 +0200

    initial commit

---
+++ src/test/java/com/demoqa/tests/LoginTests.java
@@ -0,0 +1,22 @@
+package com.demoqa.tests;
+
+import com.demoqa.pages.HomePage;
+import com.demoqa.pages.SidePanel;
+import com.demoqa.pages.bookstore.LoginPage;
+import com.demoqa.pages.bookstore.ProfilePage;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase{
+    @BeforeMethod
+    public void precondition(){
+        new HomePage(driver).getBookStore();
+        new SidePanel(driver).selectLogin();
+
+    }
+    @Test
+    public void loginPositiveTest(){
+        new LoginPage(driver).enterUserData("Yuliana", "Qwerty12345!").clickOnLoginButton();
+        new ProfilePage(driver).verifyUserName("Yuliana");
+    }
+}
